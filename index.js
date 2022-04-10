fbc7cce41f203546084d8e9d8a562185b3027e75
BSandfer <bsandfer@gmail.com>
2022-04-09 19:35:59 -0700

    fin

--- index.js
+++ index.js
@@ -54,7 +54,43 @@ const questions = [
 
 inquirer.prompt(questions)
 .then(answers => {
-    console.log(answers)
-    
+    let readMeElem = `
+<img src='https://img.shields.io/badge/license-${answers.license}-blue.svg'>
+ 
+<h1> ${answers.title} </h1>
+
+<h2> ${answers.description} </h2>
+
+<h2> Table of Contents </h2>
+
+<details open="open">
+<summary>Table of Contents</summary>
+<ol>
+<li><a href="#installation">Installation</a></li>
+<li><a href="#usage">Usage</a></li>
+<li><a href="#contributors">Contributors</a></li>
+<li><a href="#tests">Tests</a></li>
+<li><a href="#questions">Questions</a></li>
+</details>
+
+<h3 id='installation'>Installation</h3>
+<p>${answers.installation}</p>
+
+<h3 id='usage'>Usage</h3>
+<p>${answers.usage}</p>
+
+<h3 id='contributors'>Contributors</h3>
+<p>${answers.contributors}</p>
+
+<h3 id='tests'>Tests</h3>
+<p>${answers.tests}</p>
+
+<h3 id='questions'>Questions</h3>
+<p>Contact me on Github Username: ${answers.username} or via Email: ${answers.email} </p>
+
+    `
+
+
+    fs.writeFile('newReadMe.md', readMeElem, err=> console.log(err))
 })
 .catch(err=> console.log(err))
