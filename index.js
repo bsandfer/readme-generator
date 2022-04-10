a9c40fbe094a4370c7940d36b7b87725c80103fa
BSandfer <bsandfer@gmail.com>
2022-04-09 18:13:39 -0700

    init refactored

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer')
+const fs = require('fs')
 
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+const questions [
+    
+]
