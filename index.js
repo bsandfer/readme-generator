6d99abf7686c3d8afd2a1d8a6563a1777164843a
BSandfer <bsandfer@gmail.com>
2022-04-09 18:33:56 -0700

    progress

--- index.js
+++ index.js
@@ -1,6 +1,60 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
-const questions [
-    
+const questions = [
+    {
+        message: 'project title?',
+        type: 'input',
+        name: 'title'
+    },
+    {
+        message: 'project description?',
+        type: 'input',
+        name: 'description'
+    },
+    {
+        message: 'installation instructions?',
+        type: 'input',
+        name: 'installation'
+    },
+    {
+        message: 'usage instructions?',
+        type: 'input',
+        name: 'usage'
+    },
+    {
+        message: 'project license?',
+        type: 'list',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+        name: 'license'
+    },
+    {
+        message: 'contributors?',
+        type: 'input',
+        name: 'contributors'
+    },
+    {
+        message: 'tests?',
+        type: 'input',
+        name: 'tests'
+    },
+    {
+        message: 'github username?',
+        type: 'input',
+        name: 'username'
+    },
+    {
+        message: 'email?',
+        type: 'input',
+        name: 'email'
+    },
 ]
+
+
+
+inquirer.prompt(questions)
+.then(answers => {
+    console.log(answers)
+    
+})
+.catch(err=> console.log(err))
